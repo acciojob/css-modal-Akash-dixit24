e7a19915c9558483798c4e4057d9657f1f5f8c44
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2026-06-02 09:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeModal.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
